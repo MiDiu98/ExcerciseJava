d27f8831c87284ab0760f742f7641a7252572ef6
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-03-20 17:39:27 +0700

    Fix argument type for calSumDigitsOfANumber

--- src/main/java/basic/Exercise02.java
+++ src/main/java/basic/Exercise02.java
@@ -1,8 +1,9 @@
 package basic;
 
 public class Exercise02 {
-    public int calSumDigitsOfANumber(String num) {
+    public int calSumDigitsOfANumber(int number) {
 
+        String num = "" + number;
         int sum = 0;
 
         for (int index = 0; index < num.length(); index++ ) {
--- src/test/java/basic/Exercise02Test.java
+++ src/test/java/basic/Exercise02Test.java
@@ -7,8 +7,8 @@ public class Exercise02Test {
 
     @Test
     public void test_calSumDigitsOfNumber() {
-        assertEquals(new Exercise02().calSumDigitsOfANumber("0"), 0);
-        assertEquals(new Exercise02().calSumDigitsOfANumber("01234567"), 28);
-        assertEquals(new Exercise02().calSumDigitsOfANumber("1234567"), 28);
+        assertEquals(new Exercise02().calSumDigitsOfANumber(0), 0);
+        assertEquals(new Exercise02().calSumDigitsOfANumber(8790), 24);
+        assertEquals(new Exercise02().calSumDigitsOfANumber(8973), 27);
     }
 }

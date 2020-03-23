049b47ca0a755f94085de392a16979110a76c6b1
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-03-23 17:24:34 +0700

    Fix Basic/Exercise-08

--- src/main/java/basic/Exercise08.java
+++ src/main/java/basic/Exercise08.java
@@ -6,11 +6,13 @@ public class Exercise08 {
         int i = -1;
         int count = 3;
         float result = 4;
+        float add = i * 4.0f / count;
 
-        while (Math.abs(Math.PI - result) > 0.0001) {
-            result += i * 4.0f / count;
-            i *= -1;
-            count +=2;
+        while (Math.abs(add) >= 0.0001) {
+            result += add;
+            i = -i;
+            count += 2;
+            add = i * 4.0f / count;
         }
 
         return String.format("%.5f",result);
--- src/test/java/basic/Exercise08Test.java
+++ src/test/java/basic/Exercise08Test.java
@@ -6,6 +6,6 @@ import static org.junit.Assert.assertEquals;
 public class Exercise08Test {
     @Test
     public void test_calPI() {
-        assertEquals(new Exercise08().calPI(), "3.14149");
+        assertEquals(new Exercise08().calPI(), "3.14155");
     }
 }

72213e566a2d21ab47b80b5b7b50fe9935e923b6
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-03-24 16:31:02 +0700

    Fix not add file Basic/Exercise-10

--- src/main/java/basic/Exercise10.java
+++ src/main/java/basic/Exercise10.java
@@ -1,4 +1,28 @@
 package basic;
 
 public class Exercise10 {
+
+    public String findDay(int d, int m, int y) {
+        if (!checkDateValid(d, m, y)) return "Day is invalid";
+
+        final String[] DAYS =  {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        if (m < 3) {
+            m = m + 12;
+            y = y - 1;
+        }
+        int day = (d + 2 * m + 3 * (m + 1) / 5 + y + (y / 4)) % 7;
+        return DAYS[day];
+    }
+
+    public boolean checkDateValid(int d, int m, int y) {
+        if ((d < 1) || (m < 1) || (y < 1)) return false;
+        if (((y % 400 == 0) || (y % 4 == 0)) && (y % 100 != 0) && (m == 2)) {
+            if (d > 29) return false;
+            return true;
+        }
+        if ((m == 2) && (d > 28)) return  false;
+        if (((m == 4) || (m == 6) || (m == 9) || (m == 11)) && (d > 30)) return false;
+        if (d > 31) return  false;
+        return  true;
+    }
 }

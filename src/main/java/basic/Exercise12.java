9d44749a3e8e29f9bc37cf16523509d8ca602e09
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-03-25 10:51:56 +0700

    Fix Basic/Exercise-12

--- src/main/java/basic/Exercise12.java
+++ src/main/java/basic/Exercise12.java
@@ -2,9 +2,9 @@ package basic;
 
 public class Exercise12 {
 
-    public float findSquareRoot(float num) {
-        float guess = num;
-        while (Math.abs(guess * guess - num) / num >= 0.0001f)
+    public double findSquareRoot(double num) {
+        double guess = num;
+        while (Math.abs(guess * guess - num) / num >= 0.0001)
             guess = num / (2 * guess) + guess / 2;
         return guess;
     }
--- src/test/java/basic/Exercise12Test.java
+++ src/test/java/basic/Exercise12Test.java
@@ -7,7 +7,7 @@ public class Exercise12Test {
 
     @Test
     public void test_findSquareRoot() {
-        assertEquals(new Exercise12().findSquareRoot(4.0f), 2.0f, 0.0001);
-        assertEquals(new Exercise12().findSquareRoot(3.0f), 1.7321f, 0.0001);
+        assertEquals(new Exercise12().findSquareRoot(4.0), 2.0, 0.0001);
+        assertEquals(new Exercise12().findSquareRoot(3.0), 1.7321, 0.0001);
     }
 }

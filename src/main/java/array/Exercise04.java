a1a66e60bc217ebd3c64cdfec66bc172483bcb83
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-03-30 10:33:22 +0700

    Fix Array/Exercise-04

--- src/main/java/array/Exercise04.java
+++ src/main/java/array/Exercise04.java
@@ -6,32 +6,13 @@ import java.util.Collections;
 public class Exercise04 {
 
     public String sortSeparateEvenOdd(Integer[] arr) {
-        int l = 0;
-        int r = arr.length - 1;
-        int cEven = 0;
         StringBuilder res = new StringBuilder();
+        Arrays.sort(arr, 0, arr.length);
 
-        while ( l != r) {
-            while (arr[l] % 2 == 0) {
-                l++;
-                cEven++;
-            }
-
-            while ((arr[r] % 2 != 0) && (l < r)) {
-                r--;
-            }
-            if (l < r) {
-                int temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
-            }
-        }
-
-        Arrays.sort(arr, 0, cEven);
-        Arrays.sort(arr, cEven, arr.length, Collections.reverseOrder());
-
-        for (int i : arr)
-            res.append(i + " ");
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] % 2 == 0) res.append(arr[i] + " ");
+        for (int i = arr.length - 1 ; i > 0; i--)
+            if (arr[i] % 2 != 0) res.append(arr[i] + " ");
 
         return res.toString();
     }

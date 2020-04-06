bd8b42409ba9fee9be313e735692db47df8aa52f
Võ Quang Hòa <voquanghoa@gmail.com>
2020-04-06 16:22:23 +0700

    Try to make it better

--- src/main/java/string/Exercise07.java
+++ src/main/java/string/Exercise07.java
@@ -3,20 +3,22 @@ package string;
 public class Exercise07 {
     public String transToRootString(String str) {
         StringBuilder res = new StringBuilder();
+
+        str = str + " ";
+
+        char ch = '0';
+        int count = -1;
+
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isDigit(str.charAt(i))) {
-                int count = 0;
-                while ((i + count < str.length()) && Character.isDigit(str.charAt(i + count)))
-                    count++;
-                for (int j = 0; j < Integer.parseInt(str.substring(i, i + count)); j++)
-                    res.append(str.charAt(i - 1));
-                i += count - 1;
-            }
-            else if (i == str.length() - 1) {
-                res.append(str.charAt(i));
-            }
-            else if (!Character.isDigit(str.charAt(i + 1))) {
-                res.append(str.charAt(i));
+            char c = str.charAt(i);
+
+            if (Character.isDigit(c)) {
+                count = 10 * count + c - '0';
+            }else{
+                if(i > 0 && count == 0) count = 1;
+                for(int j=0; j< count; j++)res.append(ch);
+                ch = c;
+                count = 0;
             }
         }
 
--- src/test/java/string/Exercise07Test.java
+++ src/test/java/string/Exercise07Test.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class Exercise07Test {
     @Test
-    public void test_() {
+    public void test_transToRootString() {
         assertEquals(new Exercise07().transToRootString("abc4e6fd"),"abcccceeeeeefd");
         assertEquals(new Exercise07().transToRootString("ab13c"),"abbbbbbbbbbbbbc");
         assertEquals(new Exercise07().transToRootString("ab13c2"),"abbbbbbbbbbbbbcc");

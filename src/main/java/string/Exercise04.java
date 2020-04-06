4bc23a4f756aff54e5ec3f18d8a425fdb1d68d28
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-04-06 14:38:27 +0700

    Fix String/Exercise-04

--- src/main/java/string/Exercise04.java
+++ src/main/java/string/Exercise04.java
@@ -1,26 +1,20 @@
 package string;
 
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class Exercise04 {
     public boolean checkRepeatOneString(String str) {
         if (str.length() < 2) return false;
 
-        Set<String> set = new HashSet<String>();
-        for (String w : str.split(str.charAt(0) + "+")) {
-            set.add(w);
-        }
-
-        Iterator<String> iterator = set.iterator();
+        Set<String> set = new HashSet<String>(Arrays.asList(str.split(str.charAt(0) + "+")));
         int count = 0;
 
-        while (iterator.hasNext()) {
-            if (count > 1) return false;
-            String subStr = iterator.next();
-            if (!subStr.equals("")) count++;
+        for (String w : set) {
+            if (!w.equals("")) count++;
         }
-        return count > 1 ? false : true;
+
+        return count <= 1;
     }
 }
--- src/test/java/string/Exercise04Test.java
+++ src/test/java/string/Exercise04Test.java
@@ -1,15 +1,17 @@
 package string;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class Exercise04Test {
     @Test
     public void checkRepeatOneString() {
-        assertEquals(new Exercise04().checkRepeatOneString("abcdabcabc"), false);
-        assertEquals(new Exercise04().checkRepeatOneString("abcabcabc"), true);
-        assertEquals(new Exercise04().checkRepeatOneString("eeeeeeeee"), true);
-        assertEquals(new Exercise04().checkRepeatOneString("ee"), true);
-        assertEquals(new Exercise04().checkRepeatOneString("e"), false);
+        assertFalse(new Exercise04().checkRepeatOneString("abcdabcabc"));
+        assertTrue(new Exercise04().checkRepeatOneString("abcabcabc"));
+        assertTrue(new Exercise04().checkRepeatOneString("eeeeeeeee"));
+        assertTrue(new Exercise04().checkRepeatOneString("ee"));
+        assertFalse(new Exercise04().checkRepeatOneString("e"));
+        assertFalse(new Exercise04().checkRepeatOneString(""));
     }
 }

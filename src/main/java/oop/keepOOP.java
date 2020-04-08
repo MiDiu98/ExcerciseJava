14c9cbca82b48f7c18f942ddb8a2e63acb5ec50a
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-04-08 15:47:35 +0700

    OOP Exercises

---
+++ src/main/java/oop/keepOOP.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class keepOOP {
+    public String keepOOP() {
+        return "OOP";
+    }
+}
---
+++ src/test/java/oop/keepOOPTest.java
@@ -0,0 +1,11 @@
+package oop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class keepOOPTest {
+    @Test
+    public void test_keepOOP() {
+        assertEquals(new keepOOP().keepOOP(), "OOP");
+    }
+}

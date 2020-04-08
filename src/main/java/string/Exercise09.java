ed47384f8f94d9dc3404e15872f300670d97b3cc
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-04-08 14:41:19 +0700

    Fix String/Exercise-09

--- src/main/java/string/Exercise09.java
+++ src/main/java/string/Exercise09.java
@@ -1,8 +1,5 @@
 package string;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Exercise09 {
     public int findBarcodeEAN13(String str) {
             if (str.length() != 12) return -1;
@@ -11,6 +8,6 @@ public class Exercise09 {
             for (int i = 1; i < str.length(); i += 2)
                 sum += str.charAt(i - 1) + 3 * str.charAt(i) - 4* '0';
 
-            return sum % 10 == 0 ? 0 : 10 - (sum % 10);
+            return (10 - sum % 10) % 10;
     }
 }
